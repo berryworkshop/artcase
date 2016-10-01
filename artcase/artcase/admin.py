947c1cd006715d7f7c739cabe0dbcc0663d6de20
Allan Berry <allan.berry@gmail.com>
2016-10-01 13:40:12 -0500

    More model changes.

--- artcase/artcase/admin.py
+++ artcase/artcase/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import (
+    Work, Creator, Value, Location,
+    Medium, Image, Category, Collection)
 
 # Register your models here.
+
+admin.site.register(Work)
+admin.site.register(Creator)
+admin.site.register(Value)
+admin.site.register(Location)
+admin.site.register(Medium)
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Collection)
--- artcase/artcase/models.py
+++ artcase/artcase/models.py
@@ -3,15 +3,21 @@ from taggit.managers import TaggableManager
 from .fields import WhereWhenField
 from django.utils.timezone import now
 
-class Base(models.Model)
+class Base(models.Model):
     class Meta:
         abstract = True
-    created = models.DateTimeField(default=now())
+    created = models.DateTimeField(default=now)
     modified = models.DateTimeField(auto_now=True)
 
 class Work(Base):
-    title = models.CharField(max_length=100)
-    sku = models.CharField(max_length=100)
+    title = models.CharField(
+        max_length=100,
+        blank=False,
+        )
+    sku = models.CharField(
+        max_length=100,
+        blank=False,
+        )
 
     # created = WhereWhenField(max_length=100)
 
@@ -19,49 +25,100 @@ class Work(Base):
         ('cm', 'centimeters'),
         ('in', 'inches'),
     )
-    size_h = models.DecimalField(max_digits=6, decimal_places=3)
-    size_w = models.DecimalField(max_digits=6, decimal_places=3)
-    size_d = models.DecimalField(max_digits=6, decimal_places=3)
-    size_unit = models.CharField(max_length=2, choices=UNITS)
+    size_h = models.DecimalField(
+        max_digits=6,
+        decimal_places=3,
+        blank=True,
+        )
+    size_w = models.DecimalField(
+        max_digits=6,
+        decimal_places=3,
+        blank=True,
+        )
+    size_d = models.DecimalField(
+        max_digits=6,
+        decimal_places=3,
+        blank=True,
+        )
+    size_unit = models.CharField(
+        max_length=2,
+        choices=UNITS,
+        default='in',
+        blank=False,
+        )
 
     CONDITIONS = (
         ('loss', 'total loss'),
         ('poor', 'poor'),
         ('fair', 'fair'),
         ('good', 'good'),
-        ('very_good', 'very good'),
-        ('excellent', 'excellent'),
+        ('vgood', 'very good'),
+        ('excel', 'excellent'),
         )
-    condition = models.CharField(max_length=10, choices=CONDITIONS)
+    condition = models.CharField(
+        max_length=5,
+        choices=CONDITIONS,
+        blank=True,
+        )
 
     STATUSES = (
         ('?', 'missing'),
         ('display', 'on display'),
         ('storage', 'in storage'),
         )
-    status = models.CharField(max_length=10, choices=STATUSES)
-    notes = models.TextField()
+    status = models.CharField(
+        max_length=10,
+        choices=STATUSES,
+        blank=True,
+        )
+    notes = models.TextField(
+        blank=True,
+        )
 
     subjects = TaggableManager()
 
-    location = models.ForeignKey('location')
-    medium = models.ForeignKey('medium')
-
-    creators = models.ManyToManyField('creator')
-    values = models.ManyToManyField('value')
-    categories = models.ManyToManyField('category')
-    images = models.ManyToManyField('image')
-
-    collections = models.ManyToManyField('collection')
-    exhibits = models.ManyToManyField('exhibit')
+    location = models.ForeignKey(
+        'location',
+        blank=True,
+        )
+    medium = models.ForeignKey(
+        'medium',
+        blank=True,
+        )
+    creators = models.ManyToManyField(
+        'creator',
+        blank=True,
+        )
+    values = models.ManyToManyField(
+        'value',
+        blank=True,
+        )
+    categories = models.ManyToManyField(
+        'category',
+        blank=True,
+        )
+    images = models.ManyToManyField(
+        'image',
+        blank=True,
+        )
+    collections = models.ManyToManyField(
+        'collection',
+        blank=True,
+        )
 
 
 class Creator(Base):
-    name_first = models.CharField(max_length=50)
-    name_last = models.CharField(max_length=50)
+    name_first = models.CharField(
+        max_length=50,
+        blank=True,
+        )
+    name_last = models.CharField(
+        max_length=50,
+        blank=False,
+        )
 
-    #born = WhereWhenField(max_length=100)
-    #died = WhereWhenField(max_length=100)
+    #born = WhereWhenField(max_length=100, blank=True,)
+    #died = WhereWhenField(max_length=100, blank=True,)
 
 
 class Value(Base):
@@ -74,35 +131,57 @@ class Value(Base):
         ('fmv', 'fair market value'),
         ('rpv', 'replacement value'),
     )
-    value = models.DecimalField(max_digits=12, decimal_places=2)
+    value = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        blank=False,
+        )
     value_currency = models.CharField(
         max_length=3,
         choices=CURRENCIES,
         default='usd',
+        blank=False,
         )
     value_type = models.CharField(
         max_length=3,
         choices=TYPES,
         default='fmv',
+        blank=False,
         )
 
-    date = models.DateField(default=now())
+    date = models.DateField(
+        default=now,
+        blank=True,
+        )
 
 
 class Location(Base):
-    name = models.CharField(max_length=100)
-    address = models.TextField()
-    sublocation = models.TextField()
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        )
+    address = models.TextField(
+        blank=True,
+        )
+    sublocation = models.TextField(
+        blank=True,
+        )
 
 
 class Medium(Base):
-    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name_plural = "media"
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        )
 
 
 class Image(Base):
     image = models.ImageField(
         height_field='image_h',
         width_field='image_w',
+        blank=False,
         )
     image_h = models.IntegerField()
     image_w = models.IntegerField()
@@ -113,24 +192,41 @@ class Image(Base):
         ('detail','detail'),
         ('signature','signature'),
         )
-    aspect = models.CharField(max_length=10, choices=ASPECTS)
-    caption = models.TextField()
+    aspect = models.CharField(
+        max_length=10,
+        choices=ASPECTS,
+        blank=True,
+        )
+    caption = models.TextField(
+        blank=True,
+        )
 
 
 class Category(Base):
+    class Meta:
+        verbose_name_plural = "categories"
     '''
     Hierarchical organization, particular to collection
     '''
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    parent = models.ForeignKey('self', on_delete=models.CASCADE)
-    
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        )
+    description = models.TextField(
+        blank=True,
+        )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        blank=True,
+        )
+
 
 class Collection(Base):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-
-class Exhibit(Base):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(
+        max_length=100,
+        blank=False,
+        )
+    description = models.TextField(
+        blank=True,
+        )
