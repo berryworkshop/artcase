df5a9b6542ca80994c17ea2d3efcc45f38962434
Allan Berry <allan.berry@gmail.com>
2016-10-06 11:32:18 -0500

    Gulp js in effect.

--- artcase_proj/gulpfile.js
+++ artcase_proj/gulpfile.js
@@ -1,21 +1,17 @@
-/* File: gulpfile.js */
-
-// grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util'),
-    sass   = require('gulp-sass')
+    sass  = require('gulp-sass'),
+    jshint = require('gulp-jshint')
 
-gulp.task('default', function() {
-  return gutil.log('Gulp functional.')
-})
+var apps = ['core', 'artcase']
 
 gulp.task('watch', function() {
     gulp.watch('./**/*.scss', ['build_css'])
+    gulp.watch('./**/*.js',   ['build_js'])
 })
 
 gulp.task('build_css', function() {
-    var apps = ['core', 'artcase']
-
+    // each app: parallel src/*.scss & static/*.css
     apps.forEach(function(app_name) {
         var src  = './'+ app_name +'/src/**/*.scss',
             dest = './'+ app_name +'/static/'
@@ -25,6 +21,20 @@ gulp.task('build_css', function() {
     function build(src, dest) {
         return gulp.src(src)
             .pipe(sass())
+            .pipe(gulp.dest(dest))
+    } 
+})
+
+gulp.task('build_js', function() {
+    // each app: parallel src/*.js & static/*.js
+    apps.forEach(function(app_name) {
+        var src  = './'+ app_name +'/src/**/*.js',
+            dest = './'+ app_name +'/static/'
+        build(src, dest)
+    })
+
+    function build(src, dest) {
+        return gulp.src(src)
             .pipe(gulp.dest(dest))
     } 
 })
