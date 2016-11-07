db6daf0f49957fe87f63a416b54daa0961b261a5
Allan Berry <allan.berry@gmail.com>
2016-11-06 19:45:07 -0600

    View code refactored.

--- artcase_proj/artcase/urls.py
+++ artcase_proj/artcase/urls.py
@@ -9,48 +9,6 @@ from .views import (
     ArtcaseCreateView,
     ArtcaseUpdateView,
     ArtcaseDeleteView,
-
-    # WorkDetailView,
-    # WorkListView,
-    # WorkCreateView,
-    # WorkUpdateView,
-    # WorkDeleteView,
-    
-    CreatorDetailView,
-    CreatorListView,
-    CreatorCreateView,
-    CreatorUpdateView,
-    CreatorDeleteView,
-
-    LocationDetailView,
-    LocationListView,
-    LocationCreateView,
-    LocationUpdateView,
-    LocationDeleteView,
-    
-    ImageDetailView,
-    ImageListView,
-    ImageCreateView,
-    ImageUpdateView,
-    ImageDeleteView,
-
-    MediumDetailView,
-    MediumListView,
-    MediumCreateView,
-    MediumUpdateView,
-    MediumDeleteView,
-    
-    CategoryDetailView,
-    CategoryListView,
-    CategoryCreateView,
-    CategoryUpdateView,
-    CategoryDeleteView,
-    
-    CollectionDetailView,
-    CollectionListView,
-    CollectionCreateView,
-    CollectionUpdateView,
-    CollectionDeleteView,
 )
 
 urlpatterns = [
@@ -60,103 +18,154 @@ urlpatterns = [
     # works
     url(r'^work/(?P<pk>\d+)$',
         ArtcaseDetailView.as_view(
-            title = 'Work',
             model = Work
         ), name='work_detail'),
-
     url(r'^work_list$',
         ArtcaseListView.as_view(
-            title = 'List of Works',
             model = Work
         ), name='work_list'),
-    
     url(r'^work_create$', 
         ArtcaseCreateView.as_view(
-            title = 'Create a Work',
             model = Work
         ), name='work_create'),
-
     url(r'^work/(?P<pk>\d+)/update$', 
         ArtcaseUpdateView.as_view(
-            title = 'Update Work',
             model = Work
         ), name='work_update'),
-
     url(r'^work/(?P<pk>\d+)/delete$', 
         ArtcaseDeleteView.as_view(
-            title = 'Delete Work',
             model = Work
         ), name='work_delete'),
 
     # creators
     url(r'^creator/(?P<pk>\d+)$',
-        CreatorDetailView.as_view(), name='creator_detail'),
+        ArtcaseDetailView.as_view(
+            model = Creator
+        ), name='creator_detail'),
     url(r'^creator_list$',
-        CreatorListView.as_view(), name='creator_list'),
+        ArtcaseListView.as_view(
+            model = Creator
+        ), name='creator_list'),
     url(r'^creator_create$', 
-        CreatorCreateView.as_view(), name='creator_create'),
+        ArtcaseCreateView.as_view(
+            model = Creator
+        ), name='creator_create'),
     url(r'^creator/(?P<pk>\d+)/update$', 
-        CreatorUpdateView.as_view(), name='creator_update'),
+        ArtcaseUpdateView.as_view(
+            model = Creator
+        ), name='creator_update'),
     url(r'^creator/(?P<pk>\d+)/delete$', 
-        CreatorDeleteView.as_view(), name='creator_delete'),
+        ArtcaseDeleteView.as_view(
+            model = Creator
+        ), name='creator_delete'),
 
     #locations
     url(r'^location/(?P<pk>\d+)$',
-        LocationDetailView.as_view(), name='location_detail'),
+        ArtcaseDetailView.as_view(
+            model = Location
+        ), name='location_detail'),
     url(r'^location_list$',
-        LocationListView.as_view(), name='location_list'),
+        ArtcaseListView.as_view(
+            model = Location
+        ), name='location_list'),
     url(r'^location_create$', 
-        LocationCreateView.as_view(), name='location_create'),
+        ArtcaseCreateView.as_view(
+            model = Location
+        ), name='location_create'),
     url(r'^location/(?P<pk>\d+)/update$', 
-        LocationUpdateView.as_view(), name='location_update'),
+        ArtcaseUpdateView.as_view(
+            model = Location
+        ), name='location_update'),
     url(r'^location/(?P<pk>\d+)/delete$', 
-        LocationDeleteView.as_view(), name='location_delete'),
+        ArtcaseDeleteView.as_view(
+            model = Location
+        ), name='location_delete'),
     
     #images
     url(r'^image/(?P<pk>\d+)$',
-        ImageDetailView.as_view(), name='image_detail'),
+        ArtcaseDetailView.as_view(
+            model = Image
+        ), name='image_detail'),
     url(r'^image_list$',
-        ImageListView.as_view(), name='image_list'),
+        ArtcaseListView.as_view(
+            model = Image
+        ), name='image_list'),
     url(r'^image_create$', 
-        ImageCreateView.as_view(), name='image_create'),
+        ArtcaseCreateView.as_view(
+            model = Image
+        ), name='image_create'),
     url(r'^image/(?P<pk>\d+)/update$', 
-        ImageUpdateView.as_view(), name='image_update'),
+        ArtcaseUpdateView.as_view(
+            model = Image
+        ), name='image_update'),
     url(r'^image/(?P<pk>\d+)/delete$', 
-        ImageDeleteView.as_view(), name='image_delete'),
+        ArtcaseDeleteView.as_view(
+            model = Image
+        ), name='image_delete'),
     
     #media
     url(r'^medium/(?P<pk>\d+)$',
-        MediumDetailView.as_view(), name='medium_detail'),
+        ArtcaseDetailView.as_view(
+            model = Medium
+        ), name='medium_detail'),
     url(r'^medium_list$',
-        MediumListView.as_view(), name='medium_list'),
+        ArtcaseListView.as_view(
+            model = Medium
+        ), name='medium_list'),
     url(r'^medium_create$', 
-        MediumCreateView.as_view(), name='medium_create'),
+        ArtcaseCreateView.as_view(
+            model = Medium
+        ), name='medium_create'),
     url(r'^medium/(?P<pk>\d+)/update$', 
-        MediumUpdateView.as_view(), name='medium_update'),
+        ArtcaseUpdateView.as_view(
+            model = Medium
+        ), name='medium_update'),
     url(r'^medium/(?P<pk>\d+)/delete$', 
-        MediumDeleteView.as_view(), name='medium_delete'),
+        ArtcaseDeleteView.as_view(
+            model = Medium
+        ), name='medium_delete'),
     
     #categories
     url(r'^category/(?P<pk>\d+)$',
-        CategoryDetailView.as_view(), name='category_detail'),
+        ArtcaseDetailView.as_view(
+            model = Category
+        ), name='category_detail'),
     url(r'^category_list$',
-        CategoryListView.as_view(), name='category_list'),
+        ArtcaseListView.as_view(
+            model = Category
+        ), name='category_list'),
     url(r'^category_create$', 
-        CategoryCreateView.as_view(), name='category_create'),
+        ArtcaseCreateView.as_view(
+            model = Category
+        ), name='category_create'),
     url(r'^category/(?P<pk>\d+)/update$', 
-        CategoryUpdateView.as_view(), name='category_update'),
+        ArtcaseUpdateView.as_view(
+            model = Category
+        ), name='category_update'),
     url(r'^category/(?P<pk>\d+)/delete$', 
-        CategoryDeleteView.as_view(), name='category_delete'),
+        ArtcaseDeleteView.as_view(
+            model = Category
+        ), name='category_delete'),
     
     #collections
     url(r'^collection/(?P<pk>\d+)$',
-        CollectionDetailView.as_view(), name='collection_detail'),
+        ArtcaseDetailView.as_view(
+            model = Collection
+        ), name='collection_detail'),
     url(r'^collection_list$',
-        CollectionListView.as_view(), name='collection_list'),
+        ArtcaseListView.as_view(
+            model = Collection
+        ), name='collection_list'),
     url(r'^collection_create$', 
-        CollectionCreateView.as_view(), name='collection_create'),
+        ArtcaseCreateView.as_view(
+            model = Collection
+        ), name='collection_create'),
     url(r'^collection/(?P<pk>\d+)/update$', 
-        CollectionUpdateView.as_view(), name='collection_update'),
+        ArtcaseUpdateView.as_view(
+            model = Collection
+        ), name='collection_update'),
     url(r'^collection/(?P<pk>\d+)/delete$', 
-        CollectionDeleteView.as_view(), name='collection_delete'),
+        ArtcaseDeleteView.as_view(
+            model = Collection
+        ), name='collection_delete'),
 ]
--- artcase_proj/artcase/views.py
+++ artcase_proj/artcase/views.py
@@ -20,14 +20,26 @@ class FieldsMixin(object):
             'title', 'sku', 'owner',
             'size_h', 'size_w', 'size_d', 'size_unit',
             'condition', 'status', 'notes',
-            'subjects', 'location', 'medium', 'creators', 'values', 'categories', 'images', 'collections'
+            'subjects', 'location', 'medium', 'creators', 'values',
+            'categories', 'images', 'collections'
             ],
+        'creator': [
+            'first_name', 'last_name'],
+        'location': [
+            'name', 'address', 'sublocation'],
+        'image': [
+            'image', 'image_h', 'image_w', 'aspect', 'caption'],
+        'medium': [
+            'name'],
+        'category': [
+            'name', 'description', 'parent'],
+        'collection': [
+            'name', 'description'],
         }
 
     def dispatch(self, *args, **kwargs):
         self.fields = self.view_fields[self.model._meta.verbose_name]
         return super().dispatch(*args, **kwargs)
-
 
 
 class FormMixin(FieldsMixin):
@@ -39,24 +51,41 @@ class ArtcaseDetailView(LoginRequiredMixin, FieldsMixin, DetailView):
     title = None
     model = None
 
+    def dispatch(self, *args, **kwargs):
+        self.title = "{} detail: {}".format(
+            self.model._meta.verbose_name,
+            self.get_object()
+        ).title()
+        return super().dispatch(*args, **kwargs)
+
 
 class ArtcaseListView(LoginRequiredMixin, ListView):    
     template_name = 'artcase/object_list.html'
     title = None
     model = None
 
+    def dispatch(self, *args, **kwargs):
+        self.title = "{} list".format(
+            self.model._meta.verbose_name
+        ).title()
+        return super().dispatch(*args, **kwargs)
+
 class ArtcaseCreateView(LoginRequiredMixin, FieldsMixin, CreateView):
     template_name = "artcase/object_form.html"
     title = None
     model = None
 
+    def dispatch(self, *args, **kwargs):
+        self.title = "create new {}".format(
+            self.model._meta.verbose_name
+        ).title()
+        return super().dispatch(*args, **kwargs)
+
     def form_valid(self, form):
         messages.add_message(self.request, messages.SUCCESS,
             '"%s" created successfully.' % form.instance)
-
         # take note of who made it
         form.instance.created_by = self.request.user
-        
         if 'save_and_new' in form.data.keys():
             # don't redirect to list: let user create another
             reverse_string = 'artcase:{}_create'.format(
@@ -69,6 +98,13 @@ class ArtcaseUpdateView(LoginRequiredMixin, FieldsMixin, UpdateView):
     template_name = "artcase/object_form.html"
     title = None
     model = None
+
+    def dispatch(self, *args, **kwargs):
+        self.title = "update {}: {}".format(
+            self.model._meta.verbose_name,
+            self.get_object()
+        ).title()
+        return super().dispatch(*args, **kwargs)
 
     def form_valid(self, form):
         messages.add_message(self.request, messages.SUCCESS,
@@ -83,235 +119,10 @@ class ArtcaseDeleteView(LoginRequiredMixin, DeleteView):
     success_url = None
 
     def dispatch(self, *args, **kwargs):
+        self.title = "delete {}: ".format(
+            self.model._meta.verbose_name,
+            self.get_object()
+        ).title()
         self.success_url = reverse_lazy(
             'artcase:%s_list' % self.model._meta.verbose_name)
         return super().dispatch(*args, **kwargs)
-
-
-# work views
-
-# class WorkFieldsMixin(object):
-#     fields = [
-#         'title', 'sku', 'owner',
-#         'size_h', 'size_w', 'size_d', 'size_unit',
-#         'condition', 'status', 'notes',
-#         'subjects', 'location', 'medium', 'creators', 'values', 'categories', 'images', 'collections'
-#         ]
-
-
-# class WorkDetailView(WorkFieldsMixin, ArtcaseDetailView):
-#     title = 'Work'
-#     model = Work
-
-
-# class WorkListView(ArtcaseListView):    
-#     title = 'List of Works'
-#     model = Work
-
-
-# class WorkCreateView(WorkFieldsMixin, ArtcaseCreateView):
-#     title = 'Create a Work'
-#     model = Work
-
-
-# class WorkUpdateView(WorkFieldsMixin, ArtcaseUpdateView):
-#     title = "Update Work"
-#     model = Work
-
-
-# class WorkDeleteView(ArtcaseDeleteView):
-#     title = "Delete Work"
-#     model = Work
-#     success_url = reverse_lazy('artcase:work_list')
-
-
-# creator views
-
-class CreatorFieldsMixin(object):
-    fields = ['first_name', 'last_name']
-
-
-class CreatorDetailView(CreatorFieldsMixin, ArtcaseDetailView):
-    title = "Creator"
-    model = Creator
-
-
-class CreatorListView(ArtcaseListView):
-    title = 'List of Creators'
-    model = Creator
-
-
-class CreatorCreateView(CreatorFieldsMixin, ArtcaseCreateView):
-    title = 'Create a Creator'
-    model = Creator
-
-
-class CreatorUpdateView(CreatorFieldsMixin, ArtcaseUpdateView):
-    title = "Update Creator"
-    model = Creator
-
-
-class CreatorDeleteView(ArtcaseDeleteView):
-    title = "Delete Creator"
-    model = Creator
-    success_url = reverse_lazy('artcase:creator_list')
-
-
-# location views
-
-class LocationFieldsMixin(object):
-    fields = ['name', 'address', 'sublocation']
-
-
-class LocationDetailView(LocationFieldsMixin, ArtcaseDetailView):
-    title = "Location"
-    model = Location
-
-
-class LocationListView(ArtcaseListView):
-    title = 'Location List'
-    model = Location
-
-
-class LocationCreateView(LocationFieldsMixin, ArtcaseCreateView):
-    title = 'Create a Location'
-    model = Location
-
-
-class LocationUpdateView(LocationFieldsMixin, ArtcaseUpdateView):
-    title = "Update Location"
-    model = Location
-
-
-class LocationDeleteView(ArtcaseDeleteView):
-    title = "Delete Location"
-    model = Location
-    success_url = reverse_lazy('artcase:location_list')
-
-
-# image views
-
-class ImageFieldsMixin(object):
-    fields = ['image', 'image_h', 'image_w', 'aspect', 'caption']
-
-
-class ImageDetailView(ImageFieldsMixin, ArtcaseDetailView):
-    title = 'Image'
-    model = Image
-
-
-class ImageListView(ArtcaseListView):
-    title = 'Image List'
-    model = Image
-
-
-class ImageCreateView(ImageFieldsMixin, ArtcaseCreateView):
-    title = 'Create an Image'
-    model = Image
-
-
-class ImageUpdateView(ImageFieldsMixin, ArtcaseUpdateView):
-    title = "Update Image"
-    model = Image
-
-
-class ImageDeleteView(ArtcaseDeleteView):
-    title = "Delete Image"
-    model = Image
-    success_url = reverse_lazy('artcase:image_list')
-
-
-# medium views
-
-class MediumFieldsMixin(object):
-    fields = ['name']
-
-
-class MediumDetailView(MediumFieldsMixin, ArtcaseDetailView):
-    title = 'Medium'
-    model = Medium
-
-
-class MediumListView(ArtcaseListView):
-    title = 'List of Media'
-    model = Medium
-
-
-class MediumCreateView(MediumFieldsMixin, ArtcaseCreateView):
-    title = 'Create a Medium'
-    model = Medium
-
-
-class MediumUpdateView(MediumFieldsMixin, ArtcaseUpdateView):
-    title = "Update Medium"
-    model = Medium
-
-
-class MediumDeleteView(ArtcaseDeleteView):
-    title = "Delete Medium"
-    model = Medium
-    success_url = reverse_lazy('artcase:medium_list')
-
-
-# category views
-
-class CategoryFieldsMixin(object):
-    fields = ['name', 'description', 'parent']
-
-
-class CategoryDetailView(CategoryFieldsMixin, ArtcaseDetailView):
-    title = 'Category'
-    model = Category
-
-
-class CategoryListView(ArtcaseListView):
-    title = 'List of Categories'
-    model = Category
-
-
-class CategoryCreateView(CategoryFieldsMixin, ArtcaseCreateView):
-    title = 'Create a Category'
-    model = Category
-
-
-class CategoryUpdateView(CategoryFieldsMixin, ArtcaseUpdateView):
-    title = "Update Category"
-    model = Category
-
-
-class CategoryDeleteView(ArtcaseDeleteView):
-    title = "Delete Category"
-    model = Category
-    success_url = reverse_lazy('artcase:category_list')
-
-
-# collection views
-
-class CollectionFieldsMixin(object):
-    fields = ['name', 'description']
-
-
-class CollectionDetailView(CategoryFieldsMixin, ArtcaseDetailView):
-    title = 'Collection'
-    model = Collection
-
-
-class CollectionListView(ArtcaseListView):
-    title = 'List of Collections'
-    model = Collection
-
-
-class CollectionCreateView(CollectionFieldsMixin, ArtcaseCreateView):
-    title = 'Create a Collection'
-    model = Collection
-
-
-class CollectionUpdateView(CollectionFieldsMixin, ArtcaseUpdateView):
-    title = "Update Collection"
-    model = Collection
-
-
-class CollectionDeleteView(ArtcaseDeleteView):
-    title = "Delete Collection"
-    model = Collection
-    success_url = reverse_lazy('artcase:collection_list')
